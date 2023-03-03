6411538b790b21a9b6b5d39c9ad9b2c320ee2a6c
gfrnca <gabrielbfranca27@gmail.com>
2023-03-03 13:41:35 -0300

    Finished ClienteMap

--- desafio-backend/desafio-backend/Data/Map/ClienteMap.cs
+++ desafio-backend/desafio-backend/Data/Map/ClienteMap.cs
@@ -1,6 +1,19 @@
-﻿namespace desafio_backend.Data.Map
+﻿using desafio_backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace desafio_backend.Data.Map
 {
-    public class ClienteMap
+    public class ClienteMap : IEntityTypeConfiguration<ClienteEmpresarialModel>
     {
+        public void Configure(EntityTypeBuilder<ClienteEmpresarialModel> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.RazaoSocial).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.Cnpj).IsRequired().HasMaxLength(14);
+            builder.Property(x => x.Endereco).IsRequired().HasMaxLength(255);
+        }
     }
 }
