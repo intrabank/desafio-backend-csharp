89e947c9d784f70f3e36984fea4f6430b29e8ac6
gfrnca <gabrielbfranca27@gmail.com>
2023-03-03 09:47:52 -0300

    Restarting the project, created Cliente controller

---
+++ desafio-backend/desafio-backend/Models/ClienteEmpresarialModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace desafio_backend.Models
+{
+    public class ClienteEmpresarial
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        public string Endereco { get; set; }
+    }
+}
